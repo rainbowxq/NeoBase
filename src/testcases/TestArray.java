840fa2db2c5d1fdc7a3d5882ca40d31aad1e9143
zxq <rainbowxiaoqian@gmail.com>
2014-10-16 20:57:40 +0800

    fix the bindings by setting the environment

---
+++ src/testcases/TestArray.java
@@ -0,0 +1,9 @@
+package testcases;
+
+import testcases.Enum;
+public class TestArray {
+	int c[]={1,2,3};//	 *    {@link ArrayInitializer},
+    int aa=c[1];	//	 *    {@link ArrayAccess},
+    int[] ac=new int[]{1,2,3};//	 *    {@link ArrayCreation},
+    Enum haha[][];
+}
